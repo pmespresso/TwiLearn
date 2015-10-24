2773377b459f8dbc89417119af326dab29df191a
Young Joong Kim <yjkimjunior@berkeley.edu>
2015-10-24 03:15:50 -0700

    gather sms body

--- run.py
+++ run.py
@@ -8,9 +8,22 @@ def hello_monkey():
     """Respond to incoming calls with a simple text message."""
     resp = twilio.twiml.Response()
     resp.message("Text Us With What You Want To Learn. Define: { word }")
+
+    with resp.gather(action="/gather") as g:
+    	g.sms("Here you go: " )
+    resp.pause()
+    resp.redirect("/")
     return str(resp)
 
 
- 
+@app.route("/gather", methods=['GET', 'POST'])
+def gather():
+	resp = twilio.twiml.Response()
+
+	body = request.form["Body"]
+
+	resp.message(body)
+	return str(body)
+
 if __name__ == "__main__":
     app.run(debug=True)
