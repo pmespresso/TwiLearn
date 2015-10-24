8bf929b6730d44e895f8825f0128fb31b2216ef3
Young Joong Kim <yjkimjunior@berkeley.edu>
2015-10-24 04:45:32 -0700

    with utils

--- run.py
+++ run.py
@@ -1,6 +1,7 @@
 from flask import Flask, request, redirect
 import twilio.twiml
- 
+import utils
+
 app = Flask(__name__)
 # Try adding your own number to this list!
 callers = {
@@ -19,7 +20,7 @@ def hello_monkey():
 
 	resp = twilio.twiml.Response()
 	if action.upper() == "DEFINE":
-		definition = MWebLookup(value)
+		definition = utils.MWebLookup(value)
 		resp.message("The definition of " + value + "is " + definition)
 	# elif action.upper() == "SOLVE":
 	# 	resp.message("Solution is simple m8: " + value)
@@ -27,39 +28,6 @@ def hello_monkey():
 	# 	resp.message("Google it you lazy fuck " + WikiLookup(value))
 	return str(resp)
 
-def MWebLookup(s):
-	from lxml import etree
-	import requests
-	import sys
-	import re
-	page = requests.get('http://www.merriam-webster.com/dictionary/' + s)
-	text = page.text.encode('ascii', 'ignore').decode('ascii')
-	a = text.find('ssens')
-	b = text.find('>', a)
-	c = text.find('</span>', b+3)
-	#print text[b+1:c]
-	text = re.sub('(<.*?>)|(&lt)|(&gt)', '', text[b+1:c])
-	text = re.sub('\s+', ' ', text);
-	text = text[text.find(":")+1:]
-	if(text.find('{ var') != -1):
-		return 'Err: ' +s + ' could not be found. Check your spelling and try again'
-	else:
-		return text
-
-def WikiLookup(s):
-	page = requests.get('https://en.wikipedia.org/wiki/'+s)
-	text = page.text.encode('ascii', 'ignore').decode('ascii')
-	a = text.find('<p>')
-	b = text.find('>', a)
-	c = text.find('</p>', b+3)
-	#print text[b+1:c]
-	text = re.sub('(<.*?>)|(&lt)|(&gt)', '', text[b+1:c])
-	text = re.sub('\s+', ' ', text);
-	text = text[text.find(":")+1:]
-	if(text.find('{ var') != -1 or len(text) < 3):
-		return 'Err: ' +s + ' could not be found. Check your spelling and try again'
-	else:
-		return text
 
 if __name__ == "__main__":
     app.run(debug=True)
---
+++ utils.py
@@ -0,0 +1,34 @@
+from lxml import etree
+import requests
+import sys
+import re
+ 
+def MWebLookup(s):
+	page = requests.get('http://www.merriam-webster.com/dictionary/' + s)
+	text = page.text.encode('ascii', 'ignore').decode('ascii')
+	a = text.find('ssens')
+	b = text.find('>', a)
+	c = text.find('</span>', b+3)
+	#print text[b+1:c]
+	text = re.sub('(<.*?>)|(&lt)|(&gt)', '', text[b+1:c])
+	text = re.sub('\s+', ' ', text);
+	text = text[text.find(":")+1:]
+	if(text.find('{ var') != -1):
+		return 'Err: ' +s + ' could not be found. Check your spelling and try again'
+	else:
+		return text
+
+def WikiLookup(s):
+	page = requests.get('https://en.wikipedia.org/wiki/'+s)
+	text = page.text.encode('ascii', 'ignore').decode('ascii')
+	a = text.find('<p>')
+	b = text.find('>', a)
+	c = text.find('</p>', b+3)
+	#print text[b+1:c]
+	text = re.sub('(<.*?>)|(&lt)|(&gt)', '', text[b+1:c])
+	text = re.sub('\s+', ' ', text);
+	text = text[text.find(":")+1:]
+	if(text.find('{ var') != -1 or len(text) < 3):
+		return 'Err: ' +s + ' could not be found. Check your spelling and try again'
+	else:
+		return text
