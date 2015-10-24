046656a74cc2918458ced5449a50a3295dbc1eaf
Young Joong Kim <yjkimjunior@berkeley.edu>
2015-10-24 05:45:56 -0700

    dafi

--- run.py
+++ run.py
@@ -5,23 +5,44 @@ import requests
 import sys
 import re
 
-
 app = Flask(__name__)
 
+# Try adding your own number to this list!
+callers = {
+    "+14158675309": "Curious George",
+    "+14158675310": "Boots",
+    "+14158675311": "Virgil",
+}
+ 
 @app.route("/", methods=['GET', 'POST'])
 def hello_monkey():
-	message = request.values.get('Body', None)
-	message_list = message.split(" ")
+    """Respond and greet the caller by name."""
+ 
+    from_number = request.values.get('From', None)
+    if from_number in callers:
+        message = callers[from_number] + ", thanks for the message!"
+    else:
+        message = "Monkey, thanks for the message!"
+ 
+    resp = twilio.twiml.Response()
+    resp.message(message)
+ 
+    return str(resp)
 
-	action = "".join(message_list[0])
-	value = " ".join(message_list[1:])
+# @app.route("/", methods=['GET', 'POST'])
+# def hello_monkey():
+# 	message = request.values.get('Body', None)
+# 	message_list = message.split(" ")
 
-	resp = twilio.twiml.Response()
-	if action.upper() == "DEFINE":
-		resp.message("The definition of " + value + "is " + action)
-	else:
-		resp.message("nah")
-	return str(resp)
+# 	action = "".join(message_list[0])
+# 	value = " ".join(message_list[1:])
+
+# 	resp = twilio.twiml.Response()
+# 	if action.upper() == "DEFINE":
+# 		resp.message("The definition of " + value + "is " + action)
+# 	else:
+# 		resp.message("nah")
+# 	return str(resp)
 
 if __name__ == "__main__":
     app.run(debug=True)
