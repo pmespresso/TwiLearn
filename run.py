b215d9e6b3fe62df559122d1a5acf5dd301a4783
Young Joong Kim <yjkimjunior@berkeley.edu>
2015-10-24 05:40:33 -0700

    dafi

--- run.py
+++ run.py
@@ -18,7 +18,6 @@ def hello_monkey():
 
 	resp = twilio.twiml.Response()
 	if action.upper() == "DEFINE":
-		# definition = redirect('http://0.0.0.0:5000/define')
 		resp.message("The definition of " + value + "is " + action)
 	# elif action.upper() == "SOLVE":
 	# 	resp.message("Solution is simple m8: " + value)
@@ -27,37 +26,37 @@ def hello_monkey():
 	return str(resp)
 
 
-@app.route("/define", methods=['GET'])
-def mWebLookup(s):
-	page = requests.get('http://www.merriam-webster.com/dictionary/' + s)
-	text = page.text.encode('ascii', 'ignore').decode('ascii')
-	a = text.find('ssens')
-	b = text.find('>', a)
-	c = text.find('</span>', b+3)
-	#print text[b+1:c]
-	text = re.sub('(<.*?>)|(&lt)|(&gt)', '', text[b+1:c])
-	text = re.sub('\s+', ' ', text);
-	text = text[text.find(":")+1:]
-	if(text.find('{ var') != -1):
-		return 'Err: ' +s + ' could not be found. Check your spelling and try again'
-	else:
-		return text
+# @app.route("/define", methods=['GET'])
+# def mWebLookup(s):
+# 	page = requests.get('http://www.merriam-webster.com/dictionary/' + s)
+# 	text = page.text.encode('ascii', 'ignore').decode('ascii')
+# 	a = text.find('ssens')
+# 	b = text.find('>', a)
+# 	c = text.find('</span>', b+3)
+# 	#print text[b+1:c]
+# 	text = re.sub('(<.*?>)|(&lt)|(&gt)', '', text[b+1:c])
+# 	text = re.sub('\s+', ' ', text);
+# 	text = text[text.find(":")+1:]
+# 	if(text.find('{ var') != -1):
+# 		return 'Err: ' +s + ' could not be found. Check your spelling and try again'
+# 	else:
+# 		return text
 
-@app.route("/search", methods=['GET'])
-def wikiLookup(s):
-	page = requests.get('https://en.wikipedia.org/wiki/'+s)
-	text = page.text.encode('ascii', 'ignore').decode('ascii')
-	a = text.find('<p>')
-	b = text.find('>', a)
-	c = text.find('</p>', b+3)
-	#print text[b+1:c]
-	text = re.sub('(<.*?>)|(&lt)|(&gt)', '', text[b+1:c])
-	text = re.sub('\s+', ' ', text);
-	text = text[text.find(":")+1:]
-	if(text.find('{ var') != -1 or len(text) < 3):
-		return 'Err: ' +s + ' could not be found. Check your spelling and try again'
-	else:
-		return text
+# @app.route("/search", methods=['GET'])
+# def wikiLookup(s):
+# 	page = requests.get('https://en.wikipedia.org/wiki/'+s)
+# 	text = page.text.encode('ascii', 'ignore').decode('ascii')
+# 	a = text.find('<p>')
+# 	b = text.find('>', a)
+# 	c = text.find('</p>', b+3)
+# 	#print text[b+1:c]
+# 	text = re.sub('(<.*?>)|(&lt)|(&gt)', '', text[b+1:c])
+# 	text = re.sub('\s+', ' ', text);
+# 	text = text[text.find(":")+1:]
+# 	if(text.find('{ var') != -1 or len(text) < 3):
+# 		return 'Err: ' +s + ' could not be found. Check your spelling and try again'
+# 	else:
+# 		return text
 
 if __name__ == "__main__":
     app.run(debug=True)
